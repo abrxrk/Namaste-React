976699bbb9c2d4d8a4721f2b1d907d7d1c50db9e
abrxrk <akabrar5@gmail.com>
2025-06-23 13:24:01 +0530

    Saving changes to script.js before switching to main

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //JSX under the hood works exactly like react.createElement
-const jsxHeading = <h1>Hello world from React 🚀</h1>;
+const jsxHeading = () => <h1>Hello world from React 🚀</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+
+const Title = () => <h1>This is the title component wrapped in container</h1>;
+const Heading = () => (
+  <div className="container">
+    <Title />
+    <h1 className="heading">Hello world from React Functional component</h1>
+  </div>
+);
+
+root.render(<Heading />);
