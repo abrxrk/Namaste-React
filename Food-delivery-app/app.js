5e002cf2303e0b33125ce04e82c32f3629610c43
abrxrk <akabrar5@gmail.com>
2025-06-23 18:12:28 +0530

    built the basic structure of our app

--- Food-delivery-app/app.js
+++ Food-delivery-app/app.js
@@ -1,2 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+    return (
+        <div className="container">
+            
+        </div>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
