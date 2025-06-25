ed0db7a30cf919b1d7b7f1d6b42b71eb45e24314
abrxrk <akabrar5@gmail.com>
2025-06-26 00:14:09 +0530

    made small changes to resCard

--- Food-delivery-app/src/components/ResCard.js
+++ Food-delivery-app/src/components/ResCard.js
@@ -5,6 +5,8 @@ const ResCard = ({ resData }) => {
       <img className="kfc-logo" alt="res-img" src={IMG_URL}></img>
       <h3>{resData.data.name}</h3>
       <p>{resData.data.deliveryTime} minutes</p>
+      <p>{resData.data.cuisines.join(", ")}</p>
+      <p>{resData.data.avgRating} avgRating</p>
     </div>
   );
 };
